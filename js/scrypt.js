59333aeb402c6ee7e642a03c34243c2131f2a433
NikitaShevchik <shevchik.nikita@gmail.com>
2022-05-31 16:39:54 +0300

    timer(beta)

--- js/scrypt.js
+++ js/scrypt.js
@@ -8,6 +8,7 @@ const popup = document.querySelector(".popup");
 const openRules = document.querySelector(".txt__morerules");
 const popupCross = document.querySelector(".popup__cross");
 const popupBody = document.querySelector(".popup__body");
+const secondsTimer = document.querySelector(".timer__seconds");
 /*--------Функция создающая 10 рандомных чисел-----*/
 var randomItems = [];
 function generateRandoms() {
@@ -35,7 +36,7 @@ function randomSquares() {
 }
 randomSquares();
 
-var greenCounter = 0;
+
 
 boardSquareItem.forEach(e => e.addEventListener("click", function (open) {
     if (e.classList.contains("_green")) {
@@ -44,6 +45,7 @@ boardSquareItem.forEach(e => e.addEventListener("click", function (open) {
         e.classList.toggle("_green");
         greenCounter++;
         if (greenCounter == 10) {
+            clearInterval(inter);
             allBoard.classList.add("_finish");
             finishTitle.innerHTML = "Вы победили!";
             repeatGameButton.classList.remove("_hide");
@@ -72,7 +74,82 @@ popup.addEventListener("click", function (e) {
     }
 })
 
+
+var greenCounter = 0;
+var sec = 45;
+function setTimer() {
+    var inter = setInterval(tit, 1000);
+    function tit() {
+        if (allBoard.classList.contains("_finish")) {
+            clearInterval(inter);
+        }
+        if (sec >= 10) {
+            console.log(sec);
+            secondsTimer.innerHTML = `0:${sec}`;
+            sec--;
+        } else if (sec < 10) {
+            console.log(sec);
+            secondsTimer.innerHTML = `0:0${sec}`;
+            sec--;
+        }
+        if (sec == -1) {
+            console.log("Stop")
+            clearInterval(inter);
+        }
+    }
+}
+setTimer()
+
+
+
+
+
 /*--
+const seconds = document.querySelector(".timer__seconds");
+var sec = 5;
+clearInterval(countdown);
+
+function countdown() {
+    seconds.innerHTML = `${Number(seconds.innerHTML) - 1}`
+    sec -= 1;
+    console.log(sec);
+    if (sec == 0) {
+        clearInterval(countdown);
+        console.log('sho')
+    }
+};
+
+
+
+
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+    if (minutes === '00' && seconds === '00'){
+        console.log("NO TIME")
+    }
+}
+window.onload = function () {
+    var oneMinute = 60,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+};
+
 function randomTen(e){
     for(let i = 0; i < 10; i++){
         for (let i = Math.floor(Math.random() * 100); i < boardSquareItem.length; i++){
